3cbd66799c0759afc10066623c74b319b39da527
Mikhail Naumovich <144479173+Flexmatik@users.noreply.github.com>
2024-04-30 17:19:17 +0300

    Update hausdorff_cpp.h

--- hausdorff_cpp.h
+++ hausdorff_cpp.h
@@ -11,31 +11,30 @@ inline double SquaredDistance(double x0, double y0, double x1, double y1) {
 
 inline std::array<double, 8> Hausdorff_CPP(NormalBox_cpp box,
                                            PackedPoint_cpp pack) {
-  std::array<double, 8> X;
-  std::array<double, 8> Y;
+  const std::array<double, 8> X;
+  const std::array<double, 8> Y;
   for (int i = 0; i < 8; i++) {
     X[i] = std::abs(pack.X[i]);
     Y[i] = std::abs(pack.Y[i]);
   }
   std::array<double, 8> result;
   for (int i = 0; i < 8; i++) {
-    if (X[i] <= box.w && Y[i] <= box.h) {
+    if (X[i] <= box.Half_Width && Y[i] <= box.Half_Height) {
 
-      double a1 = SquaredDistance(X[i], Y[i], -box.w, box.h);
-      double a2 = SquaredDistance(X[i], Y[i], -box.w, -box.h);
-      double a3 = SquaredDistance(X[i], Y[i], box.w, -box.h);
+      result[i] =
+          SquaredDistance(X[i], Y[i], -box.Half_Width, -box.Half_Height);
+    } else if (X[i] <= box.Half_Width && Y[i] > box.Half_Height) {
 
-      result[i] = (std::max(std::max(a1, a2), a3));
-    } else if (X[i] <= box.w && Y[i] > box.h) {
+      result[i] =
+          (SquaredDistance(X[i], Y[i], -box.Half_Width, box.Half_Height));
+    } else if (X[i] > box.Half_Width && Y[i] <= box.Half_Height) {
 
-      result[i] = (SquaredDistance(X[i], Y[i], -box.w, box.h));
-    } else if (X[i] > box.w && Y[i] <= box.h) {
-
-      result[i] = (SquaredDistance(X[i], Y[i], box.w, -box.h));
+      result[i] =
+          (SquaredDistance(X[i], Y[i], box.Half_Width, -box.Half_Height));
     } else {
 
-      double a1 = SquaredDistance(X[i], Y[i], -box.w, box.h);
-      double a2 = SquaredDistance(X[i], Y[i], box.w, -box.h);
+      const double a1 = SquaredDistance(X[i], Y[i], -box.Half_Width, box.Half_Height);
+      const double a2 = SquaredDistance(X[i], Y[i], box.Half_Width, -box.Half_Height);
 
       result[i] = (std::max(a1, a2));
     }
