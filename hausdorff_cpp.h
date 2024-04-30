7537ea6e3a12c603da56510c50de0b313b33d3bc
Mikhail Naumovich <144479173+Flexmatik@users.noreply.github.com>
2024-04-30 17:35:17 +0300

    Update hausdorff_cpp.h

--- hausdorff_cpp.h
+++ hausdorff_cpp.h
@@ -5,14 +5,14 @@
 #include <iostream>
 
 inline double SquaredDistance(double x0, double y0, double x1, double y1) {
-  double squared = (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
+  const double squared = (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
   return squared;
 }
 
 inline std::array<double, 8> Hausdorff_CPP(NormalBox_cpp box,
                                            PackedPoint_cpp pack) {
-  const std::array<double, 8> X;
-  const std::array<double, 8> Y;
+  std::array<double, 8> X;
+  std::array<double, 8> Y;
   for (int i = 0; i < 8; i++) {
     X[i] = std::abs(pack.X[i]);
     Y[i] = std::abs(pack.Y[i]);
