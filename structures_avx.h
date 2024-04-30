b7d2e10c02bcb99b9caa3bc1def8836e879bcd9b
Mikhail Naumovich <144479173+Flexmatik@users.noreply.github.com>
2024-04-30 17:24:41 +0300

    Update structures_avx.h

--- structures_avx.h
+++ structures_avx.h
@@ -6,8 +6,7 @@ struct PackedPoint_avx {
   __m512d y;
 };
 
-struct
-    NormalBox_avx { // coordinates of the point in the first quarter
-  double w;
-  double h;
+struct NormalBox_avx { // coordinates of the point in the first quadrant
+  double Half_Width;
+  double Half_Height;
 };
